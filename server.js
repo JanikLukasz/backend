6f61302eccc23c6f597f33e0ac443b74f9399898
Lukasz Janik <j.lukaszmaria@gmail.com>
2023-10-05 19:34:53 +0200

    first commit

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const itemsRoutes = require("./routes/items");
 const categoriesRoutes = require("./routes/categories");
+const cors = require('cors');
+
 
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
